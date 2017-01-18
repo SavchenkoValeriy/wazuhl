850d31770ad1f9bf107d3e1dfb94f1f140c3a525
Valeriy Savchenko <sinmipt@ispras.ru>
2017-01-19 01:34:34 +0300

    Issue #28. Add neural network creation.
    This commit is only to check that wazuhl is properly
    linked with caffe library.

--- lib/Wazuhl/DQN.cpp
+++ lib/Wazuhl/DQN.cpp
@@ -1,2 +1,15 @@
 #include "llvm/Wazuhl/DQN.h"
 #include <caffe/caffe.hpp>
+
+namespace llvm {
+namespace wazuhl {
+  class DQNImpl {
+  public:
+    DQN::Result calculate(const DQN::State &S, const DQN::Action &A) {
+      caffe::Caffe::set_mode(caffe::Caffe::GPU);
+      caffe::Net<double> NN{"model_file", caffe::TEST};
+      return 0.0;
+    }
+  };
+}
+}
