d2fab10c82af5157770feacaeeb23492384a0377
Valeriy Savchenko <Valeriy.Savchenko@roguewave.com>
2017-02-22 15:52:48 +0300

    Issue #36. Change ExperienceReplay signatures.

--- include/llvm/Wazuhl/ExperienceReplay.h
+++ include/llvm/Wazuhl/ExperienceReplay.h
@@ -1,6 +1,7 @@
 #ifndef LLVM_WAZUHL_EXPERIENCEREPLAY_H
 #define LLVM_WAZUHL_EXPERIENCEREPLAY_H
 
+#include "llvm/Wazuhl/DQN.h"
 #include "llvm/ADT/SmallVector.h"
 #include <utility>
 
@@ -12,9 +13,8 @@ namespace llvm { namespace wazuhl {
   public:
     static constexpr unsigned int MinibatchSize = 32;
 
-    using Result = double;
-    using ExperienceUnit = std::pair<const FeatureVector &,
-                                     Result>;
+    using ExperienceUnit = std::pair<DQNCore::State,
+                                     DQNCore::ResultsVector>;
     using RecalledExperience = SmallVector<ExperienceUnit,
                                            MinibatchSize>;
 
