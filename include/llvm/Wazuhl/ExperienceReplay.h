291a1f81f69287b5f2af963eb43ffa70df1a342b
Valeriy Savchenko <vsavchenko@ispras.ru>
2018-03-31 18:04:07 +0300

    Fix bug with forgotten namespace.

--- include/llvm/Wazuhl/ExperienceReplay.h
+++ include/llvm/Wazuhl/ExperienceReplay.h
@@ -1,6 +1,7 @@
 #ifndef LLVM_WAZUHL_EXPERIENCEREPLAY_H
 #define LLVM_WAZUHL_EXPERIENCEREPLAY_H
 
+#include "llvm/Wazuhl/Config.h"
 #include "llvm/Wazuhl/DQN.h"
 #include "llvm/ADT/SmallVector.h"
 #include <utility>
@@ -18,7 +19,7 @@ namespace llvm { namespace wazuhl {
                                      DQNCore::ResultsVector>;
     using State = DQNCore::State;
     using RecalledExperience = SmallVector<ExperienceUnit,
-                                           MinibatchSize>;
+                                           config::MinibatchSize>;
 
     void addToExperience(ExperienceUnit);
     void addToExperience(State terminal);
