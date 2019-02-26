5f3398229c1e90502e86414c79a90427e24f8c99
Valeriy Savchenko <vsavchenko@ispras.ru>
2019-02-26 19:18:36 +0300

    Fix policy evaluator for DQN
    Change-Id: I8947acfc8dac3cb6df8637008c715e144b72688e

--- lib/Wazuhl/PolicyEvaluator.cpp
+++ lib/Wazuhl/PolicyEvaluator.cpp
@@ -1,23 +1,23 @@
 #include "llvm/Wazuhl/PolicyEvaluator.h"
-#include "llvm/Wazuhl/Environment.h"
 #include "llvm/Wazuhl/DQN.h"
+#include "llvm/Wazuhl/Environment.h"
 #include "llvm/Wazuhl/ReinforcementLearning.h"
 
 namespace llvm {
 namespace wazuhl {
-  void PolicyEvaluator::evaluate() {
-    DQN Q;
-    rl::policies::Greedy<DQN> policy{Q};
-    auto learner = rl::createLearner<rl::NonLearning>(OptimizationEnv, Q, policy);
-    learner.learn();
-  }
+void PolicyEvaluator::evaluate() {
+  DQN Q;
+  rl::policies::Greedy<DQN> policy{Q};
+  auto learner = rl::createLearner<rl::NonLearning>(OptimizationEnv, Q, policy);
+  learner.learn();
+}
 
-  void LearningPolicyEvaluator::evaluate() {
-    DQN Q;
-    rl::policies::EpsilonGreedy<DQN> policy{0.7, Q};
-    auto learner = rl::createLearner<rl::QLearning>(OptimizationEnv, Q,
-                                                    policy, 0.1, 0.99);
-    learner.learn();
-  }
+void LearningPolicyEvaluator::evaluate() {
+  DQN Q;
+  rl::policies::EpsilonGreedy<DQN> policy{0.9, Q};
+  auto learner =
+      rl::createLearner<rl::QLearning>(OptimizationEnv, Q, policy, 1.0, 0.99);
+  learner.learn();
 }
-}
+} // namespace wazuhl
+} // namespace llvm
